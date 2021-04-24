ebaec655a427dc20647c9abdd268fbc3ce44c029
Darwin <darwinblacks@gmail.com>
2021-04-24 13:42:31 +0700

    feat: complete game

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -9,6 +9,12 @@ public class Player : MonoBehaviour
     private bool onLeft, onRight;
     private bool jumped;
 
+    [SerializeField]
+    AudioSource audioKill, audioJump;
+    [SerializeField]
+    private AudioClip deadSound;
+    private bool isAlive = true;
+
     private void Awake()
     {
         GameObject.Find("JumpBtn").GetComponent<Button>().onClick.AddListener(Jump);
@@ -27,19 +33,39 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!jumped)
+        if (isAlive)
         {
-            if (onRight)
+            if (!jumped)
             {
-                anim.Play("RunRight");
+                if (onRight)
+                {
+                    anim.Play("RunRight");
+                }
+                else
+                {
+                    anim.Play("RunLeft");
+                }
             }
-            else
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                anim.Play("RunLeft");
+                if (onRight)
+                {
+                    anim.Play("JumpLeft");
+                }
+                else
+                {
+                    anim.Play("JumpRight");
+                }
+                jumped = true;
+                audioJump.Play();
             }
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    public void Jump()
+    {
+        if (isAlive)
         {
             if (onRight)
             {
@@ -50,20 +76,8 @@ public class Player : MonoBehaviour
                 anim.Play("JumpRight");
             }
             jumped = true;
+            audioJump.Play();
         }
-    }
-
-    public void Jump()
-    {
-        if (onRight)
-        {
-            anim.Play("JumpLeft");
-        }
-        else
-        {
-            anim.Play("JumpRight");
-        }
-        jumped = true;
     }
 
     void OnLeft()
@@ -78,5 +92,48 @@ public class Player : MonoBehaviour
         onLeft = false;
         onRight = true;
         jumped = false;
+    }
+
+    void PlayerDied()
+    {
+        audioKill.clip = deadSound;
+        audioKill.Play();
+
+        if (transform.position.x > 0)
+        {
+            anim.Play("PlayerDiedRight");
+        }
+        else
+        {
+            anim.Play("PlayerDiedLeft");
+        }
+
+        GameplayController.instance.GameOver();
+
+        Time.timeScale = 0f;
+    }
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if (jumped)
+        {
+            if (target.tag == "Enemy")
+            {
+                target.gameObject.SetActive(false);
+                audioKill.Play();
+            }
+        }
+        else
+        {
+            if (target.tag == "Enemy")
+            {
+                PlayerDied();
+            }
+        }
+
+        if (target.tag == "EnemyTree")
+        {
+            PlayerDied();
+        }
     }
 }
